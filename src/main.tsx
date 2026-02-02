a9787cc15964f9f60bf636ed68656e8ffa412230
Roberto Miranda Morales <rob.mirandam@gmail.com>
2026-02-02 11:53:31 -0600

    [feature] agregando sistema de rutas con react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+//import { StrictMode } from 'react'
+//import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.tsx'
+import { MainContainer } from "@/forms/main-container"
+import { SecondaryContainer } from "@/forms/secondary-container"
+import "./App.css"
+import './index.css'
+
+//createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>,)
+
+const root: HTMLElement | null = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/main-form" element={<MainContainer />} />
+          <Route path="/second-form" element={<SecondaryContainer />} />
+      </Routes>
+  </BrowserRouter>,
+);
